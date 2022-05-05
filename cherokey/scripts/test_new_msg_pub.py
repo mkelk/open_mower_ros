69eef219f72eab01495153b4bd9fe6535e69e5df
Morten Elk <morten@elk.dk>
2022-05-05 22:52:01 +0200

    Talking to rosserial node with a custom message

--- cherokey/scripts/test_new_msg_pub.py
+++ cherokey/scripts/test_new_msg_pub.py
@@ -7,19 +7,18 @@ from cherokey_msgs.msg import WheelState
 def talker():
     
     pub = rospy.Publisher("test_msg_topic", String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-
-    pub_wheels = rospy.Publisher("test_msg_cherokey_wheels", WheelState, queue_size=10)
+    pub_wheels = rospy.Publisher("wheels_set_state", WheelState, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     
     
-    rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
         hello_str = "hello world %s" % rospy.get_time()
         rospy.loginfo(f"Sending {hello_str}")
         pub.publish(hello_str)
-        pub_wheels.publish(10,1,100,-1)
-        rate.sleep()
+        pub_wheels.publish(100,1,100,1)
+        rospy.sleep(1.0)
+        pub_wheels.publish(100,-1,100,-1)
+        rospy.sleep(1.0)
 
 if __name__ == '__main__':
     try:
